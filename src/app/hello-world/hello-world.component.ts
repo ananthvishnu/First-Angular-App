8b786821d1c3115960cf09f8dc05cb7e062b2b46
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2023-08-03 20:49:39 +0530

    ngFor Directive learned

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -6,6 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./hello-world.component.css']
 })
 export class HelloWorldComponent {
-firstName = "vishnu";
-lastName = "rathan";
+  // firstName = "vishnu";
+  // color = "red"
+
+
+  // clickCount = 0;
+  // clickCount2 = 0;
+
+  // clickMe() {
+  //   this.clickCount++;
+  // }
+
+// event la irunthu data assign panni view pannuthal
+// value = ""
+// value1 = ""
+// handleInput(event:any){
+// this.value = (event.target as HTMLInputElement).value
+// }
+  
+title = "List of Users";
+users = [
+  {
+    "name": "Alice",
+    "age": 25,
+    "occupation": "Teacher",
+    "location": "Chicago"
+  },
+  {
+    "name": "Bob",
+    "age": 32,
+    "occupation": "Software Engineer",
+    "location": "San Francisco"
+  },
+  {
+    "name": "Eva",
+    "age": 40,
+    "occupation": "Doctor",
+    "location": "London"
+  },
+  {
+    "name": "Michael",
+    "age": 28,
+    "occupation": "Writer",
+    "location": "Paris"
+  },
+  {
+    "name": "Sophia",
+    "age": 22,
+    "occupation": "Student",
+    "location": "Sydney"
+  }
+  
+  
+]
+
+
 }
